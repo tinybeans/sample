c4049c55a3f6f7511f1f113b9c2a7acd31915d3b
Roy Okuwaki <register@tinybeans.net>
2022-08-28 15:55:27 +1200

    Changed to function

--- 2019-03-11/user.js
+++ 2019-03-11/user.js
@@ -1,6 +1,16 @@
-// テキスト（複数行）をオーバーレイエディタに変更する
 (function($){
-    const value = $('#content-field-94').val();
-    $('#contentField94 .mt-contentblock').html('<textarea name="content-field-94"></textarea>');
-    $('[name="content-field-94"]').val(value).mtapp('overlayEditor');
+
+    // テキスト（複数行）をオーバーレイエディタに変更する
+    function convertOverlay(fieldId) {
+        const name = `content-field-${fieldId}`;
+        const value = $(`[name="${name}"]`).attr('name', '').val();
+        const $container = $(`#contentField${fieldId}`).hide();
+        const label = $container.find('label').first().html().trim();
+        $container.after(
+            `<div class="form-group"><label>${label}</label><textarea name="${name}"></textarea></div>`
+        ).hide();
+        $(`[name="${name}"]`).val(value).mtapp('overlayEditor');
+    }
+    convertOverlay(242);
+
 })(jQuery);
